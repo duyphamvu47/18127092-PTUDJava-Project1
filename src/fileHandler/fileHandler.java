d4b7f8abd5ee75ac9705adba8290e5e7903144c3
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2020-11-17 17:39:24 +0700

    Thêm hàm ghi file + Chỉnh lại dữ liệu trong file slang.txt

--- src/fileHandler/fileHandler.java
+++ src/fileHandler/fileHandler.java
@@ -4,24 +4,30 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class fileHandler {
-    public static void read(String fileName){
-        try(BufferedReader br = new BufferedReader(new FileReader(new File(fileName)))) {
-            String line = br.readLine();
-            // ArrayList<String, String> a = new ArrayList<String, String>();
-            while ((line = br.readLine()) != null){
-                String[] preprocess = line.split("`");
+
+    public static void inputFileHandler(String Infile, String Outfile){
+        try(BufferedReader br = new BufferedReader(new FileReader(new File(Infile)));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File(Outfile)))) {
+
+            String lineIn = br.readLine();
+
+            while ((lineIn = br.readLine()) != null){
+                String[] preprocess = lineIn.split("`");
                 String slang = preprocess[0];
                 String[] meaning = preprocess[1].split("\\| ");
                 for(String temp : meaning){
-                    System.out.println(slang + ", " + temp);
+                    bw.write(slang + "|" + temp + "\n");
                 }
             }
+
             br.close();
+            bw.close();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     public static void main(String[] args) {
-        read("input.txt");
+        inputFileHandler("slang.txt", "Preprocessed.txt");
     }
 }
